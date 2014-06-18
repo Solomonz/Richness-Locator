adae539d3b24fced19501428b71031ff81a76b97
Solomon Zitter <technowiz@Sol-Zitters-MacBook-Pro.local>
2014-06-17 19:14:54 -0700

    Initial Commit

---
+++ Richness Locator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Richness Locator
+//
+//  Created by Solomon Zitter on 6/17/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Richness LocatorTests/Richness_LocatorTests.m
@@ -0,0 +1,34 @@
+//
+//  Richness_LocatorTests.m
+//  Richness LocatorTests
+//
+//  Created by Solomon Zitter on 6/17/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Richness_LocatorTests : XCTestCase
+
+@end
+
+@implementation Richness_LocatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
